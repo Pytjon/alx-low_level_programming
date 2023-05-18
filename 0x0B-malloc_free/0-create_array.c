6ca48a0ee8ae13470047fcfb8084644c93b1dcef
Pytjon <adebolaoluwadare19@gmail.com>
2023-05-18 06:16:23 -0700

    updated the function

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,14 +13,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	arr = malloc(sizeof(char) * size);
-	arr[0] = 'A';
-
 	if (size == 0 || arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
 
 	return (arr);
-	free(arr);
-	return (0);
 }
