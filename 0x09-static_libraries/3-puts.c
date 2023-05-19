2c34b6fd50570695b49f4eae52e2f38fc76fda67
Pytjon <adebolaoluwadare19@gmail.com>
2023-05-19 07:23:33 -0700

    creating a shared library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string to the standard output
+ * @s: The string to be printed
+ */
+void _puts(char *s)
+{
+	printf("%s\n", s);
+}
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
