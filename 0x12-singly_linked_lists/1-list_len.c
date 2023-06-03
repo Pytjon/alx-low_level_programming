b657133e5280bc20f3747265a306ccde37cdb42c
Pytjon <adebolaoluwadare19@gmail.com>
2023-06-02 19:54:38 -0700

    prints number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - finds the no of elements in a linked list
+ * @h: the linked list
+ * Return: the no of elements in h
+ */
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+	return (elements);
+}
