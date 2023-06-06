83665c6100af1f42627146c5df39e5b64e1965eb
Pytjon <adebolaoluwadare19@gmail.com>
2023-06-06 05:39:05 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,30 +10,25 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *new, *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if (!new || !head)
+	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = NULL;
 	if (idx == 0)
 	{
-		new->next = *head;
+		new->next = temp;
 		*head = new;
 		return (new);
 	}
-	for (i = 0; temp && i < idx; i++)
+	for (i = 0; i < (idx - 1); i++)
 	{
-		if (i == idx - 1)
-		{
-			new->next = temp->next;
-			temo->next = new;
-			return (new);
-		}
-		else
-			temp = temp->next;
+		if (temp == NULL || temp->next == NULL)
+			return (NULL);
+		temp = temp->next;
 	}
+	new->next = temp->next;
+	temp->next = new;
 	return (new);
 }
