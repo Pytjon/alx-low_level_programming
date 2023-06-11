00f658b5e67432b37c036dda87135b9eab5da7fd
Pytjon <adebolaoluwadare19@gmail.com>
2023-06-11 16:08:17 -0700

    converts binary to unsignrd integer

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ * Return: the converted number
+ * or 0 if b is NULL; chars in the string that is not 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec_int = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_int = 2 * dec_int + (b[i] - '0');
+	}
+	return (dec_int);
+}
